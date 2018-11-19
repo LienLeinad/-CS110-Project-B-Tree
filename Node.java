689b671a03306b91b4a7d12fbae37c2e7d816146
MadridMAC <miguel.madrid@obf.ateneo.edu>
2018-11-19 20:00:33 +0800

    Minor update to Node.java

--- Node.java
+++ Node.java
@@ -2,8 +2,16 @@ import java.util.*;
 import java.io.*;
 public class Node
 {
-	public Node
+	private long[] keyArray;
+	
+	public Node()
 	{
-		
+		// node record consists of 14 "pointers" of type long int
+		keyArray = new long[14];
+		// initialize -1 values within the long[] array
+		for (int i = 0; i < 14; i++)
+		{
+			keyArray[i] = -1;
+		}
 	}
 }
